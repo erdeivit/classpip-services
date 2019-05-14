133bd4b7f172113c318690f51bb9dfa71bf216f6
David <erdeivit@gmail.com>
2019-05-14 10:23:33 +0200

    Creado QUESTIONARIO FLIPCARD con Provincias España

--- server/boot/09-create-questionnaires.js
+++ server/boot/09-create-questionnaires.js
@@ -4,7 +4,7 @@ module.exports = function (app, cb) {
 
   app.models.QuestionnaireGame.create([{
       id: 1,
-      name: 'El juego de la semana',
+      name: 'El juego de la semana. "Quizpip"',
       start_date: '2019-03-02T13:10:27.162Z',
       finish_date: '2019-04-02T13:10:27.162Z',
       question_time: '15',
@@ -15,12 +15,12 @@ module.exports = function (app, cb) {
       teamMode: 0
     }, {
       id: 2,
-      name: 'El juego de las restas',
+      name: 'El juego de las restas. "1by1"',
       start_date: '2019-03-02T13:10:27.162Z',
       finish_date: '2019-05-02T13:10:27.162Z',
       question_time: '10',
       questionnaire_time: '100',
-      groupId: 2,
+      groupId: 1,
       teacherId: 1000,
       gameMode: '1by1',
       teamMode: 1
@@ -28,16 +28,27 @@ module.exports = function (app, cb) {
     },
     {
       id: 3,
-      name: 'Potenciame',
+      name: 'Potenciame. "1by" all poss',
       start_date: '2019-03-02T13:10:27.162Z',
       finish_date: '2019-05-02T13:10:27.162Z',
       question_time: '10',
       questionnaire_time: '100',
       groupId: 1,
       teacherId: 1000,
-      gameMode: 'Quizpip',
+      gameMode: '1by1',
       teamMode: 0
-
+    },
+    {
+      id: 4,
+      name: 'Capitales de provincias de España',
+      start_date: '2019-05-14T13:10:27.162Z',
+      finish_date: '2019-05-02T13:10:27.162Z',
+      question_time: '10',
+      questionnaire_time: '100',
+      groupId: 1,
+      teacherId: 1000,
+      gameMode: 'FlipcardsPip',
+      teamMode: 0
     }
   ], function (err, questionnaireGames) {
     if (err) throw err;
@@ -66,6 +77,14 @@ module.exports = function (app, cb) {
       questionId: [6, 7, 8],
       questionnaireGameId: 3,
       teacherId: 1000
+    }, {
+      id: 4,
+      name: 'Aprender geografia.',
+      description: 'Vamos a repasar las capitales de provincia de españa',
+      image: 'https://profesorpaco.files.wordpress.com/2015/03/mapa-mundo-provincia-y-capitales-espac3b1olas.jpg',
+      questionId: [9, 10, 11],
+      questionnaireGameId: 3,
+      teacherId: 1000
     }], function (err, questionnaires) {
       if (err) throw err;
       // Assign questionnaires to the questionnairesGame
@@ -75,158 +94,218 @@ module.exports = function (app, cb) {
           if (err) throw err;
           questionnaireGames[2].questionnaire.add(questionnaires[2], function (err) {
             if (err) throw err;
-            app.models.Question.create([{
-                id: 1,
-                statement: '2+2 = ?',
-                answer1: "2",
-                answer2: "4",
-                answer3: "6",
-                answer4: "8",
-                correctanswer: "4",
-                image: 'https://respuestas.tips/wp-content/uploads/2013/07/suma.jpg',
-                difficulty: 'easy',
-                category: 'operations',
-                explanation: 'La suma de 2+2 siempre da 4.',
-                questionnaireId: 1,
-                teacherId: 1000,
-                type: "classic"
-              },
-              {
-                id: 2,
-                statement: '9+4 = ?',
-                answer1: "11",
-                answer2: "13",
-                answer3: "15",
-                answer4: "12",
-                correctanswer: "13",
-                image: 'https://matematicasmodernas.com/wp-content/uploads/2014/07/suma-de-vectores-por-componentes.jpg',
-                difficulty: 'medium',
-                category: 'operations',
-                explanation: 'La suma de 9+4 siempre da 13.',
-                questionnaireId: 1,
-                teacherId: 1000,
-                type: "classic"
-
-              },
-              {
-                id: 3,
-                statement: '10-6 = ?',
-                answer1: "7",
-                answer2: "0",
-                answer3: "16",
-                answer4: "4",
-                correctanswer: "4",
-                image: 'https://matematicasmodernas.com/wp-content/uploads/2014/09/Resta-de-vectores.jpg',
-                difficulty: 'hard',
-                category: 'operations',
-                explanation: 'La resta de 10-6 siempre da 4.',
-                questionnaireId: [1, 2],
-                teacherId: 1000,
-                type: "classic"
-              },
-              {
-                id: 4,
-                statement: '100-39 = ?',
-                answer1: "61",
-                answer2: "50",
-                answer3: "139",
-                answer4: "60",
-                correctanswer: "61",
-                image: 'https://matematicasmodernas.com/wp-content/uploads/2014/09/Resta-de-vectores.jpg',
-                difficulty: 'hard',
-                category: 'operations',
-                explanation: 'La resta de 100-39 siempre da 61.',
-                questionnaireId: [1, 2],
-                teacherId: 1000,
-                type: "classic"
-              },
-              {
-                id: 5,
-                statement: '20-11= ?',
-                answer1: "7",
-                answer2: "8",
-                answer3: "9",
-                answer4: "10",
-                correctanswer: "9",
-                image: 'https://matematicasmodernas.com/wp-content/uploads/2014/09/Resta-de-vectores.jpg',
-                difficulty: 'medium',
-                category: 'operations',
-                explanation: 'La resta de 20-11 siempre da 9.',
-                questionnaireId: [1, 2],
-                teacherId: 1000,
-                type: "classic"
-              },
-              {
-                id: 6,
-                statement: 'sqrt(4)',
-                answer1: "+2",
-                answer2: "-2",
-                answer3: "+4",
-                answer4: "-4",
-                correctanswer: "+2,-2",
-                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Racine_carr%C3%A9e_bleue.svg/220px-Racine_carr%C3%A9e_bleue.svg.png',
-                difficulty: 'high',
-                category: 'power',
-                explanation: 'La raiz siempre nos da dos valores. +-2',
-                questionnaireId: 3,
-                teacherId: 1000,
-                type: "multianswer"
-              },
-              {
-                id: 7,
-                statement: 'Cuanto es 2^3',
-                answer1: "8",
-                correctanswer: "8",
-                image: 'https://static.escolakids.uol.com.br/conteudo_legenda/81f0bcf8fd3124a9d0eb33048107bc8c.jpg',
-                difficulty: 'high',
-                category: 'power',
-                explanation: '2x2x2 = 8',
-                questionnaireId: 3,
-                teacherId: 1000,
-                type: "openAnswer"
-              },
-              {
-                id: 8,
-                statement: 'Cuanto es -3^2',
-                answer1: "9",
-                answer2: "-9",
-                answer3: "6",
-                answer4: "-6",
-                correctanswer: "9",
-                image: 'https://es-static.z-dn.net/files/dbb/0f6b68bdb84c4bbc16cf532d5c19865c.jpg',
-                difficulty: 'high',
-                category: 'power',
-                explanation: '(-3)*(-3) = 9',
-                questionnaireId: 3,
-                teacherId: 1000,
-                type: "classic"
-              }
-            ], function (err, q) {
+            questionnaireGames[3].questionnaire.add(questionnaires[3], function (err) {
               if (err) throw err;
-              // Assign correct answers to questions
-              questionnaires[0].question.add(q[0], function (err) {
+              app.models.Question.create([{
+                  id: 1,
+                  statement: '2+2 = ?',
+                  answer1: "2",
+                  answer2: "4",
+                  answer3: "6",
+                  answer4: "8",
+                  correctanswer: "4",
+                  image: 'https://respuestas.tips/wp-content/uploads/2013/07/suma.jpg',
+                  difficulty: 'easy',
+                  category: 'operations',
+                  explanation: 'La suma de 2+2 siempre da 4.',
+                  questionnaireId: 1,
+                  teacherId: 1000,
+                  type: "classic"
+                },
+                {
+                  id: 2,
+                  statement: '9+4 = ?',
+                  answer1: "11",
+                  answer2: "13",
+                  answer3: "15",
+                  answer4: "12",
+                  correctanswer: "13",
+                  image: 'https://matematicasmodernas.com/wp-content/uploads/2014/07/suma-de-vectores-por-componentes.jpg',
+                  difficulty: 'medium',
+                  category: 'operations',
+                  explanation: 'La suma de 9+4 siempre da 13.',
+                  questionnaireId: 1,
+                  teacherId: 1000,
+                  type: "classic"
+
+                },
+                {
+                  id: 3,
+                  statement: '10-6 = ?',
+                  answer1: "7",
+                  answer2: "0",
+                  answer3: "16",
+                  answer4: "4",
+                  correctanswer: "4",
+                  image: 'https://matematicasmodernas.com/wp-content/uploads/2014/09/Resta-de-vectores.jpg',
+                  difficulty: 'hard',
+                  category: 'operations',
+                  explanation: 'La resta de 10-6 siempre da 4.',
+                  questionnaireId: [1, 2],
+                  teacherId: 1000,
+                  type: "classic"
+                },
+                {
+                  id: 4,
+                  statement: '100-39 = ?',
+                  answer1: "61",
+                  answer2: "50",
+                  answer3: "139",
+                  answer4: "60",
+                  correctanswer: "61",
+                  image: 'https://matematicasmodernas.com/wp-content/uploads/2014/09/Resta-de-vectores.jpg',
+                  difficulty: 'hard',
+                  category: 'operations',
+                  explanation: 'La resta de 100-39 siempre da 61.',
+                  questionnaireId: [1, 2],
+                  teacherId: 1000,
+                  type: "classic"
+                },
+                {
+                  id: 5,
+                  statement: '20-11= ?',
+                  answer1: "7",
+                  answer2: "8",
+                  answer3: "9",
+                  answer4: "10",
+                  correctanswer: "9",
+                  image: 'https://matematicasmodernas.com/wp-content/uploads/2014/09/Resta-de-vectores.jpg',
+                  difficulty: 'medium',
+                  category: 'operations',
+                  explanation: 'La resta de 20-11 siempre da 9.',
+                  questionnaireId: [1, 2],
+                  teacherId: 1000,
+                  type: "classic"
+                },
+                {
+                  id: 6,
+                  statement: 'sqrt(4)',
+                  answer1: "+2",
+                  answer2: "-2",
+                  answer3: "+4",
+                  answer4: "-4",
+                  correctanswer: "+2,-2",
+                  image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Racine_carr%C3%A9e_bleue.svg/220px-Racine_carr%C3%A9e_bleue.svg.png',
+                  difficulty: 'hard',
+                  category: 'power',
+                  explanation: 'La raiz siempre nos da dos valores. +-2',
+                  questionnaireId: 3,
+                  teacherId: 1000,
+                  type: "multianswer"
+                },
+                {
+                  id: 7,
+                  statement: 'Cuanto es 2^3',
+                  answer1: "8",
+                  correctanswer: "8",
+                  image: 'https://static.escolakids.uol.com.br/conteudo_legenda/81f0bcf8fd3124a9d0eb33048107bc8c.jpg',
+                  difficulty: 'hard',
+                  category: 'power',
+                  explanation: '2x2x2 = 8',
+                  questionnaireId: 3,
+                  teacherId: 1000,
+                  type: "openAnswer"
+                },
+                {
+                  id: 8,
+                  statement: 'Cuanto es -3^2',
+                  answer1: "9",
+                  answer2: "-9",
+                  answer3: "6",
+                  answer4: "-6",
+                  correctanswer: "9",
+                  image: 'https://es-static.z-dn.net/files/dbb/0f6b68bdb84c4bbc16cf532d5c19865c.jpg',
+                  difficulty: 'hard',
+                  category: 'power',
+                  explanation: '(-3)*(-3) = 9',
+                  questionnaireId: 3,
+                  teacherId: 1000,
+                  type: "classic"
+                },
+                {
+                  id: 9,
+                  statement: 'Capital de provincia de Cataluña',
+                  answer1: "Barcelona",
+                  answer2: "Tarragona",
+                  answer3: "Lérida",
+                  answer4: "Girona",
+                  correctanswer: "Barcelona",
+                  image: 'www.gifex.com/images/500X0/2010-09-16-12148/Las-provincias-de-Cataluna.jpg',
+                  difficulty: 'easy',
+                  category: 'capitales',
+                  explanation: 'La capital de Cataluña es Barcelona',
+                  questionnaireId: 4,
+                  teacherId: 1000,
+                  type: "classic"
+                },
+                {
+                  id: 10,
+                  statement: 'Capital de provincia de Extremadura',
+                  answer1: "Cáceres",
+                  answer2: "Badajoz",
+                  answer3: "Mérida",
+                  correctanswer: "Mérida",
+                  image: 'www.gifex.com/images/500X0/2010-09-16-12148/Las-provincias-de-Cataluna.jpg',
+                  difficulty: 'easy',
+                  category: 'capitales',
+                  explanation: 'La capital de Extremadura es Mérida',
+                  questionnaireId: 4,
+                  teacherId: 1000,
+                  type: "classic"
+                },
+                {
+                  id: 11,
+                  statement: 'Provincias de Andalucia',
+                  answer1: "Sevilla",
+                  answer2: "Jaén",
+                  answer3: "Soria",
+                  answer4: "Chiclana de la Frontera",
+                  correctanswer: "Sevilla,Jaén",
+                  image: 'www.gifex.com/images/500X0/2010-09-16-12148/Las-provincias-de-Cataluna.jpg',
+                  difficulty: 'easy',
+                  category: 'capitales',
+                  explanation: 'La capital de Extremadura es Mérida',
+                  questionnaireId: 4,
+                  teacherId: 1000,
+                  type: "multianswer"
+                }
+              ], function (err, q) {
                 if (err) throw err;
-                questionnaires[0].question.add(q[1], function (err) {
+                // Assign correct answers to questions
+                questionnaires[0].question.add(q[0], function (err) {
                   if (err) throw err;
-                  questionnaires[0].question.add(q[2], function (err) {
+                  questionnaires[0].question.add(q[1], function (err) {
                     if (err) throw err;
-                    questionnaires[0].question.add(q[3], function (err) {
+                    questionnaires[0].question.add(q[2], function (err) {
                       if (err) throw err;
-                      questionnaires[0].question.add(q[4], function (err) {
+                      questionnaires[0].question.add(q[3], function (err) {
                         if (err) throw err;
-                        questionnaires[1].question.add(q[2], function (err) {
+                        questionnaires[0].question.add(q[4], function (err) {
                           if (err) throw err;
-                          questionnaires[1].question.add(q[3], function (err) {
+                          questionnaires[1].question.add(q[2], function (err) {
                             if (err) throw err;
-                            questionnaires[1].question.add(q[4], function (err) {
+                            questionnaires[1].question.add(q[3], function (err) {
                               if (err) throw err;
-                              questionnaires[2].question.add(q[5], function (err) {
+                              questionnaires[1].question.add(q[4], function (err) {
                                 if (err) throw err;
-                                questionnaires[2].question.add(q[6], function (err) {
+                                questionnaires[2].question.add(q[5], function (err) {
                                   if (err) throw err;
-                                  questionnaires[2].question.add(q[7], function (err) {
+                                  questionnaires[2].question.add(q[6], function (err) {
                                     if (err) throw err;
-                                    process.nextTick(cb);
+                                    questionnaires[2].question.add(q[7], function (err) {
+                                      if (err) throw err;
+                                      questionnaires[3].question.add(q[8], function (err) {
+                                        if (err) throw err;
+                                        questionnaires[3].question.add(q[9], function (err) {
+                                          if (err) throw err;
+                                          questionnaires[3].question.add(q[10], function (err) {
+                                            if (err) throw err;
+
+                                            process.nextTick(cb);
+                                          });
+                                        });
+                                      });
+                                    });
                                   });
                                 });
                               });
